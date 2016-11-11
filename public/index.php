b0dbf5a34712c7a7dace53aa8d5308167bb21428
Santos Gallegos <santos_g@outlook.com>
2016-11-11 16:13:59 -0500

    Ejemplo de template

--- public/index.php
+++ public/index.php
@@ -1,3 +1,12 @@
 <?php
-echo '<h1>Hola mundo!</h1>';
-?>
+include_once '../app/init.php';
+
+use Aluc\Service\TemplateGenerator;
+
+$valores = array(
+    'title' => 'Test - Template',
+    'body' => 'Hola mundo!'
+);
+
+TemplateGenerator::generate($valores, 'template_ejemplo.php');
+
---
+++ resources/templates/template_ejemplo.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo $body;
+        ?>
+    </h1>
+</body>
+</html>
