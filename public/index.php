2dbcab41d535465d580ac940b0c41be0106b95c1
Santos Gallegos <santos_g@outlook.com>
2016-11-12 19:21:31 -0500

    Redirect urls from index.php

--- public/index.php
+++ public/index.php
@@ -1,11 +1,32 @@
 <?php
 include_once '../app/init.php';
 
-use Aluc\Service\TemplateGenerator;
+/**
+ * The following function will strip the script name from URL.
+ * (copy/paste, no tocar!)
+*/
+function getCurrentUri() {
+    $basepath = implode('/', array_slice(explode('/', $_SERVER['SCRIPT_NAME']), 0, -1)) . '/';
+    $uri = substr($_SERVER['REQUEST_URI'], strlen($basepath));
+    if (strstr($uri, '?')) {
+        $uri = substr($uri, 0, strpos($uri, '?'));
+    }
+    $uri = '/' . trim($uri, '/');
+    return $uri;
+}
 
-$valores = array(
-    'title' => 'Test - Template',
-    'body' => 'Hola mundo!'
-);
+$base_url = getCurrentUri();
+$routes = array();
+$routes = explode('/', $base_url);
 
-TemplateGenerator::generate($valores, 'template_ejemplo.php');
+/*
+Now, $routes will contain all the routes. $routes[0] will correspond to first route (/).
+*/
+
+if($routes[1] == 'admin') {
+    if ($routes[2] == 'moderadores') {
+        echo 'Administrar Moreadores :)';
+    }
+} else {
+    echo "Página de Inicio";
+}
