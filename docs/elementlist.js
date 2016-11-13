4892deeda16a71d62bc547d5adf7f825c7089414
Santos Gallegos <santos_g@outlook.com>
2016-11-13 10:08:46 -0500

    Generada documentación

---
+++ docs/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Aluc\\Dao\\Database"],["c","Aluc\\Dao\\ModeradorDao"],["c","Aluc\\Modulo\\Usuario"],["c","Aluc\\Service\\Administrador"],["c","Aluc\\Service\\DBItem"],["c","Aluc\\Service\\Fecha"],["c","Aluc\\Service\\Horario"],["c","Aluc\\Service\\Laboratorio"],["c","Aluc\\Service\\Lector"],["c","Aluc\\Service\\Moderador"],["c","Aluc\\Service\\Persona"],["c","Aluc\\Service\\Reserva"],["c","Aluc\\Service\\Usuario"],["c","Aluc\\Tools\\TemplateGenerator"],["c","Aluc\\Tools\\Urls"],["c","Aluc\\Views\\ModeradoresView"]];
