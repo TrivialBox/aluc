a5bc17d639787252a13ea23159842865ca569f57
Santos Gallegos <santos_g@outlook.com>
2016-11-12 20:22:36 -0500

    add Tools namespace

---
+++ app/Aluc/Tools/Urls.php
@@ -0,0 +1,31 @@
+<?php
+namespace Aluc\Tools;
+
+class Urls {
+    private static function get_url() {
+        $base_url = static::get_current_uri();
+        return $base_url;
+        // $routes = array();
+        // $routes = explode('/', $base_url);
+    }
+
+    private static function get_current_uri() {
+        // Copy % paste, no tocar!
+        $basepath = implode('/', array_slice(explode('/', $_SERVER['SCRIPT_NAME']), 0, -1)) . '/';
+        $uri = substr($_SERVER['REQUEST_URI'], strlen($basepath));
+        if (strstr($uri, '?')) {
+            $uri = substr($uri, 0, strpos($uri, '?'));
+        }
+        $uri = '/' . trim($uri, '/');
+        return $uri;
+    }
+
+    public static function serve_request($urls) {
+        $base_url = static::get_url();
+        foreach ($urls as $url => $func) {
+            if (preg_match($url, $base_url)) {
+                $func();
+            }
+        }
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,40 +1,15 @@
 <?php
 include_once '../app/init.php';
 
-function get_url() {
-    $base_url = get_current_uri();
-    return $base_url;
-    // $routes = array();
-    // $routes = explode('/', $base_url);
-}
+use Aluc\Tools\Urls;
 
-function get_current_uri() {
-    // Copy % paste, no tocar!
-    $basepath = implode('/', array_slice(explode('/', $_SERVER['SCRIPT_NAME']), 0, -1)) . '/';
-    $uri = substr($_SERVER['REQUEST_URI'], strlen($basepath));
-    if (strstr($uri, '?')) {
-        $uri = substr($uri, 0, strpos($uri, '?'));
-    }
-    $uri = '/' . trim($uri, '/');
-    return $uri;
-}
-
-function serve_request($urls) {
-    $base_url = get_url();
-    foreach ($urls as $url => $func) {
-        if (preg_match($url, $base_url)) {
-            $func();
-        }
-    }
-}
-
-serve_request(
-    array(
-        '/^\/admin\/moderadores/i' => 'funcion_saludar'
-    )
-);
 
 function funcion_saludar() {
     echo 'Hola mundo';
 }
 
+Urls::serve_request(
+    array(
+        '/^\/admin\/moderadores/i' => 'funcion_saludar'
+    )
+);
