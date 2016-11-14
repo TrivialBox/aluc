1b097a1a8bbf5ae23f88925ed198f22b9d3eacf5
LaraFranklin <larafranklin95@gmail.com>
2016-11-14 09:43:39 -0500

    Merge ModeradorDAO

--- app/Aluc/Dao/ModeradorDao.php
+++ app/Aluc/Dao/ModeradorDao.php
@@ -3,26 +3,31 @@ namespace Aluc\Dao;
 
 
 
-class ModeradorDao {
+class ModeradorDao
+{
     private $database;
     private static $instance = null;
 
-    private function __construct() {
+    private function __construct()
+    {
         $this->database = new Database();
     }
 
-    public static function getInstance() {
+    public static function getInstance()
+    {
         if (static::$instance == null) {
             static::$instance = new self();
         }
         return static::$instance;
     }
 
-    public function save($obj){
-        return $this->database->insert("insert_moderador",$obj);
+    public function save($obj)
+    {
+        return $this->database->insert("insert_moderador", $obj);
     }
 
-    public function get($cedula){
+    public function get($cedula)
+    {
         $where = "id = " . "'" . $cedula . "'";
         $moderador = $this->database->select("moderador", "*", $where, null);
         return $moderador;
@@ -30,16 +35,13 @@ class ModeradorDao {
 
     public function getAll($order_atribute){
         $order_by = null;
-
-        if ($order_atribute != null){
-            $order_by = $order_atribute . " asc";
+        if ($order_atribute != null) {
+        $order_by = $order_atribute . " asc";
         }
         /* si se usa una vista especificar el nombre el el primer parametro del metodo select
         *  de igual manera si es una tabla solo poner el nombre de la tabla
         */
         $list_moderador = $this->database->select("moderador", "*", null, $order_by);
-
         return $list_moderador;
-
     }
 }
