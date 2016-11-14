321bbf8b3a619cb1e6bd069da5965bae9fe8278f
Santos Gallegos <santos_g@outlook.com>
2016-11-14 00:37:39 -0500

    Update index

--- public/index.php
+++ public/index.php
@@ -3,38 +3,28 @@ session_start();
 include_once '../app/init.php';
 
 use Aluc\Tools\Urls;
-use Aluc\Views\ModeradoresView;
+use Aluc\Service\AdministradorSrv;
+use Aluc\Service\ErrorSrv;
 
-
-function moderadores() {
-    if (
-        isset($_SESSION['id']) && !empty($_SESSION['id']) &&
-        isset($_SESSION['type']) && $_SESSION['type'] === 'admin'
-    ) {
-        echo "welcome!";
-    } else {
-        die("NO");
-    }
-    ModeradoresView::getInstance()->listAll()->render();
-}
 
 function home() {
     echo 'Página de inicio';
 }
 
-function not_found() {
-    include '404.html';
-}
-
-function login() {
-    include 'login.php';
-}
 
 Urls::serve_request(
     array(
-        '/^\/$/' => 'home',
-        '/^\/login$/i' => 'login',
-        '/^\/admin\/moderadores$/i' => 'moderadores',
-        '/.*/' => 'not_found'
+        '/^\/$/' => function() {
+            return home();
+        },
+        '/^\/error\/404$/i' => function() {
+            return ErrorSrv::error404();
+        },
+        '/^\/admin\/moderadores$/i' => function() {
+            return AdministradorSrv::moderadores();
+        },
+        '/.*/' => function() {
+            return ErrorSrv::error404();
+        }
     )
 );
