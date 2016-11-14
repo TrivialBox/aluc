cc757fee234f5d69006a0daaa9b29dce4642a20c
Santos Gallegos <santos_g@outlook.com>
2016-11-14 00:23:39 -0500

    No typehint

--- app/Aluc/Tools/TemplateGenerator.php
+++ app/Aluc/Tools/TemplateGenerator.php
@@ -4,8 +4,10 @@ namespace Aluc\Tools;
 define('TEMPLATES_PATH', __DIR__ . '/../../../resources/templates');
 
 class TemplateGenerator {
-    public static function generate(array $values, $template_name) {
-        extract($values);
+    public static function generate($values, $template_name) {
+        if (is_array($values)) {
+            extract($values);
+        }
         include TEMPLATES_PATH . "/{$template_name}";
     }
 }
