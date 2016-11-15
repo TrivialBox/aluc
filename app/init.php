5f068c8b3b07886d2833b4338dd60cf5cb5791ae
Santos Gallegos <santos_g@outlook.com>
2016-11-15 03:57:05 -0500

    Errores -> Excepciones

--- app/init.php
+++ app/init.php
@@ -1,3 +1,8 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
+// Tratar todos los errores como excepciones.
+function exception_error_handler($errno, $errstr, $errfile, $errline ) {
+    throw new Exception($errstr, $errno, 0, $errfile, $errline);
+}
+set_error_handler("exception_error_handler");
