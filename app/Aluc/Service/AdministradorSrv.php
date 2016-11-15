3f377490492dd0dd2b86d8f8825bbc85d9cc2632
Santos Gallegos <santos_g@outlook.com>
2016-11-14 21:40:55 -0500

    Implementado crud de lectores & moderadores

--- app/Aluc/Service/AdministradorSrv.php
+++ app/Aluc/Service/AdministradorSrv.php
@@ -21,8 +21,48 @@ class AdministradorSrv {
         static::$view_lector_qr = LectorQrView::getInstance();
     }
 
+    private static function admin_do($func, $view_success, $view_error) {
+        $view = null;
+        try {
+            if (Tools::check_session('admin')) {
+                if (Tools::check_method('post')) {
+                    $func();
+                }
+                $view = $view_success();
+            } else {
+                $view = self::$view_general->error404();
+            }
+        } catch (\Exception $e) {
+            $view = $view_error();
+        } finally {
+            $view->render();
+        }
+    }
+
+    private static function admin_go($view_go, $view_error) {
+        $view = null;
+        try {
+            if (Tools::check_session('admin')) {
+                $view = $view_go();
+            } else {
+                $view = self::$view_general->error404();
+            }
+        } catch (\Exception $e) {
+            $view  = $view_error();
+        } finally {
+            $view->render();
+        }
+    }
+
+    /**
+     * /admin
+     *
+     * Muestra la página principal del administrador
+     * desde la cual tiene acceso a varias herramientas
+     * de administración.
+     */
     public static function home() {
-        // página principal del administrador
+        // TODO: página principal del administrador
     }
 
     /**
@@ -32,18 +72,14 @@ class AdministradorSrv {
      * Sólo el administrador tiene acceso.
      */
     public static function moderadores() {
-        $view = null;
-        try {
-            if (Tools::check_session('admin')) {
-                $view = self::$view_moderador->listAll();
-            } else {
-                $view = self::$view_general->error404();
+        self::admin_go(
+            function () {
+                return self::$view_moderador->listAll();
+            },
+            function () {
+                return self::$view_general->error404();
             }
-        } catch (\Exception $e) {
-            $view  = self::$view_general->error404();
-        } finally {
-            $view->render();
-        }
+        );
     }
 
 
@@ -55,26 +91,70 @@ class AdministradorSrv {
      * Sólo un administrador puede realizar esta acción.
      */
     public static function moderadores_nuevo() {
-        $view = null;
-        try {
-            if (Tools::check_session('admin')) {
-                if (Tools::check_method('post')) {
-                    $id = Tools::clean_string($_POST['id']);
-                    $laboratorio_id = Tools::clean_string($_POST['laboratorio_id']);
-                    $moderador = Moderador::getNewInstace($id, $laboratorio_id);
-                    $moderador->save();
-                }
-                $view = self::$view_moderador->listAll();
-            } else {
-                $view = self::$view_general->error404();
+        self::admin_do(
+            function () {
+                $id = Tools::clean_string($_POST['id']);
+                $laboratorio_id = Tools::clean_string($_POST['laboratorio_id']);
+                $moderador = Moderador::getNewInstace($id, $laboratorio_id);
+                $moderador->save();
+            },
+            function () {
+                return self::$view_moderador->listAll();
+            },
+            function () {
+                return self::$view_general->error404();
             }
-        } catch (\Exception $e) {
-            $view = self::$view_general->error404();
-        } finally {
-            $view->render();
-        }
+        );
     }
 
+    /**
+     * /admin/moderadores/actualizar
+     *
+     * Editar la información de un moderador.
+     * La petición se debe hacer vía post.
+     * Sólo un administrador puede realizar esta acción.
+     */
+    public static function moderadores_actualizar() {
+        self::admin_do(
+            function () {
+                $id = Tools::clean_string($_POST['id']);
+                $laboratorio_id = Tools::clean_string($_POST['laboratorio_id']);
+                $moderador = Moderador::getInstance($id);
+                $moderador->id_laboratorio = $laboratorio_id;
+                $moderador->save();
+            },
+            function () {
+                return self::$view_moderador->listAll();
+            },
+            function () {
+                return self::$view_general->error404();
+            }
+        );
+    }
+
+
+    /**
+     * /admin/moderadores/eliminar
+     *
+     * Elimina un moderador del sistema.
+     * La petición se debe hacer vía post.
+     * Sólo un administrador puede realizar esta acción.
+     */
+    public static function moderadores_eliminar() {
+        self::admin_do(
+            function () {
+                $id = Tools::clean_string($_POST['id']);
+                $moderador = Moderador::getInstance($id);
+                $moderador->delete();
+            },
+            function () {
+                return self::$view_moderador->listAll();
+            },
+            function () {
+                return self::$view_general->error404();
+            }
+        );
+    }
 
     /**
      * /admin/lectores
@@ -83,18 +163,14 @@ class AdministradorSrv {
      * Sólo el administrador tiene acceso.
      */
     public static function lectores() {
-        $view = null;
-        try {
-            if (Tools::check_session('admin')) {
-                $view = self::$view_lector_qr->listAll();
-            } else {
-                $view = self::$view_general->error404();
+        self::admin_go(
+            function () {
+                return self::$view_lector_qr->listAll();
+            },
+            function () {
+                return self::$view_general->error404();
             }
-        } catch (\Exception $e) {
-            $view = self::$view_general->error404();
-        } finally {
-            $view->render();
-        }
+        );
     }
 
 
@@ -106,25 +182,78 @@ class AdministradorSrv {
      * Sólo un administrador puede realizar esta acción.
      */
     public static function lectores_nuevo() {
-        $view = null;
-        try {
-            if (Tools::check_session('admin')) {
-                if (Tools::check_method('post')) {
-                    $ip = Tools::clean_string($_POST['ip']);
-                    $mac = Tools::clean_string($_POST['mac']);
-                    $laboratorio_id = Tools::clean_string($_POST['laboratorio_id']);
-                    $lector = Lector::getNewInstance($ip, $mac, $laboratorio_id);
-                    $lector->save();
-                }
-                $view = self::$view_moderador->listAll();
-            } else {
-                $view = self::$view_general->error404();
+        self::admin_do(
+            function () {
+                $ip = Tools::clean_string($_POST['ip']);
+                $mac = Tools::clean_string($_POST['mac']);
+                $laboratorio_id = Tools::clean_string($_POST['laboratorio_id']);
+                $lector = Lector::getNewInstance($ip, $mac, $laboratorio_id);
+                $lector->save();
+            },
+            function () {
+                return self::$view_moderador->listAll();
+            },
+            function () {
+                return self::$view_general->error404();
             }
-        } catch (\Exception $e) {
-            $view = self::$view_general->error404();
-        } finally {
-            $view->render();
-        }
+        );
+    }
+
+    /**
+     * /admin/lectores/actualizar
+     *
+     * Edita la información de un lector.
+     * Si el valor de new_token es true se genera un nuevo
+     * token para el lector.
+     * La petición se debe hacer vía post.
+     * Sólo un administrador puede realizar esta acción.
+     */
+    public static function lectores_actualizar() {
+        self::admin_do(
+            function () {
+                $id = Tools::clean_string($_POST['id']);
+                $ip = Tools::clean_string($_POST['ip']);
+                $mac = Tools::clean_string($_POST['mac']);
+                $laboratorio_id = Tools::clean_string($_POST['laboratorio_id']);
+                $new_token = strtolower(Tools::clean_string($_POST['new_token'])) === 'true';
+                $lector = Lector::getInstance($id);
+                $lector->ip = $ip;
+                $lector->mac = $mac;
+                $lector->laboratorio_id = $laboratorio_id;
+                if ($new_token) {
+                    $lector->renovarToken();
+                }
+                $lector->save();
+            },
+            function () {
+                return self::$view_lector_qr->listAll();
+            },
+            function () {
+                return self::$view_general->error404();
+            }
+        );
+    }
+
+    /**
+     * Elimina un lector del sistema.
+     * La petición se debe hacer vía post.
+     * Sólo un administrador puede realizar esta acción.
+     */
+    public static function lectores_eliminar() {
+        self::admin_do(
+            function () {
+                $id = Tools::clean_string($_POST['id']);
+                $laboratorio_id = Tools::clean_string($_POST['laboratorio_id']);
+                $lector = Lector::getInstance($id, $laboratorio_id);
+                $lector->delete();
+            },
+            function () {
+                return self::$view_lector_qr->listAll();
+            },
+            function () {
+                return self::$view_general->error404();
+            }
+        );
     }
 }
 
