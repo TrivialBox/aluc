59ca8d75733feb12874ed08df63d5a08eb6999c3
krysthyan <krysthyan_09h@yahoo.com>
2016-11-15 04:24:32 -0500

    fix error exception

--- app/init.php
+++ app/init.php
@@ -3,6 +3,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 // Tratar todos los errores como excepciones.
 function exception_error_handler($errno, $errstr, $errfile, $errline ) {
-    throw new Exception($errstr, $errno, 0, $errfile, $errline);
+    throw new Exception($errstr, $errno);
 }
 set_error_handler("exception_error_handler");
