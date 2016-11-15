43f5b266aaf0c75f14f2769f059e05a18bea8518
Santos Gallegos <santos_g@outlook.com>
2016-11-15 03:39:36 -0500

    Claves empiezan desde 1

--- database/populate.sql
+++ database/populate.sql
@@ -304,22 +304,22 @@ INSERT INTO usuario (id, nombre) VALUES ('0105070353', 'SANCHEZ BELTRAN JORGE DA
 INSERT INTO usuario (id, nombre) VALUES ('1400967715', 'SANCHEZ CALLE JASSON RUBEN');
 
 -- Jornadas
-INSERT INTO jornada (id, hora_apertura, hora_cierre) VALUES ('0', '07:00', '13:00');
-INSERT INTO jornada (id, hora_apertura, hora_cierre) VALUES ('1', '09:00', '13:00');
+INSERT INTO jornada (id, hora_apertura, hora_cierre) VALUES ('1', '07:00', '13:00');
+INSERT INTO jornada (id, hora_apertura, hora_cierre) VALUES ('2', '09:00', '13:00');
 
-INSERT INTO jornada (id, hora_apertura, hora_cierre) VALUES ('2', '15:00', '17:00');
-INSERT INTO jornada (id, hora_apertura, hora_cierre) VALUES ('3', '15:30', '17:30');
+INSERT INTO jornada (id, hora_apertura, hora_cierre) VALUES ('3', '15:00', '17:00');
+INSERT INTO jornada (id, hora_apertura, hora_cierre) VALUES ('4', '15:30', '17:30');
 
 -- Horarios
-INSERT INTO horario (id, id_jornada1, id_jornada2) VALUES ('0', '0', '2');
-INSERT INTO horario (id, id_jornada1, id_jornada2) VALUES ('1', '0', '3');
-INSERT INTO horario (id, id_jornada1, id_jornada2) VALUES ('2', '1', '2');
-INSERT INTO horario (id, id_jornada1, id_jornada2) VALUES ('3', '1', '3');
+INSERT INTO horario (id, id_jornada1, id_jornada2) VALUES ('1', '1', '3');
+INSERT INTO horario (id, id_jornada1, id_jornada2) VALUES ('2', '1', '4');
+INSERT INTO horario (id, id_jornada1, id_jornada2) VALUES ('3', '2', '3');
+INSERT INTO horario (id, id_jornada1, id_jornada2) VALUES ('4', '2', '4');
 
 -- Laboratorios
-INSERT INTO laboratorio (id, nombre, capacidad, descripcion, id_horario) VALUES ('0', 'Lab. Física', '15', 'Laboratorio de física para clases de física.', '0');
-INSERT INTO laboratorio (id, nombre, capacidad, descripcion, id_horario) VALUES ('1', 'Lab. Química', '20', 'Laboratorio de química para clases de química.', '1');
-INSERT INTO laboratorio (id, nombre, capacidad, descripcion, id_horario) VALUES ('2', 'Lab. Suelos', '10', 'Laboratorio de suelos para los de civil.', '2');
-INSERT INTO laboratorio (id, nombre, capacidad, descripcion, id_horario) VALUES ('3', 'Lab. Máquinas', '12', 'Laboratorio de máquinas para los de civil.', '3');
-INSERT INTO laboratorio (id, nombre, capacidad, descripcion, id_horario) VALUES ('4', 'Lab. de computo', '25', 'Laboratorio que en realidad son salas de computo.', '0');
-INSERT INTO laboratorio (id, nombre, capacidad, descripcion, id_horario) VALUES ('5', 'Lab. de python', '10', 'Laboratorio de python, con la ayuda de Guido Van Rossum.', '1');
+INSERT INTO laboratorio (id, nombre, capacidad, descripcion, id_horario) VALUES ('1', 'Lab. Física', '15', 'Laboratorio de física para clases de física.', '1');
+INSERT INTO laboratorio (id, nombre, capacidad, descripcion, id_horario) VALUES ('2', 'Lab. Química', '20', 'Laboratorio de química para clases de química.', '2');
+INSERT INTO laboratorio (id, nombre, capacidad, descripcion, id_horario) VALUES ('3', 'Lab. Suelos', '10', 'Laboratorio de suelos para los de civil.', '3');
+INSERT INTO laboratorio (id, nombre, capacidad, descripcion, id_horario) VALUES ('4', 'Lab. Máquinas', '12', 'Laboratorio de máquinas para los de civil.', '4');
+INSERT INTO laboratorio (id, nombre, capacidad, descripcion, id_horario) VALUES ('5', 'Lab. de computo', '25', 'Laboratorio que en realidad son salas de computo.', '1');
+INSERT INTO laboratorio (id, nombre, capacidad, descripcion, id_horario) VALUES ('6', 'Lab. de python', '10', 'Laboratorio de python, con la ayuda de Guido Van Rossum.', '2');
