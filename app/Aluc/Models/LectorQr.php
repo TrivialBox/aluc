497b736d70873d59f0aa74d386cbd37ee0cc70e5
LaraFranklin <larafranklin95@gmail.com>
2016-11-19 14:05:26 -0500

    Creado DAO del lector del QR

--- app/Aluc/Models/LectorQr.php
+++ app/Aluc/Models/LectorQr.php
@@ -1,26 +1,32 @@
 <?php
 namespace Aluc\Models;
+use ALUC\Dao\LectorDao;
 
 /**
  * Representación de un lector de códigos QR.
  */
 class LectorQr {
-    public $id;
-    public $ip;
+
     public $mac;
+    public $ip;
     public $laboratorio_id;
 
     private $token;
 
-    private function __construct($mac, $ip, $token, $laboratorio_id) {
+    private function __construct($mac, $ip, $laboratorio_id, $token) {
         $this->ip = $ip;
         $this->mac = $mac;
         $this->token = $token;
         $this->laboratorio_id = $laboratorio_id;
     }
 
-    public static function getNewInstance($mac, $ip, $laboratorio_id) {
+    private static function get_object($array, $get_element = true){
 
+            return 1;
+    }
+
+    public static function getNewInstance($mac, $ip, $laboratorio_id) {
+        return LectorQr::get_object(LectorDao::getInstance());
     }
 
     public static function getInstance($mac) {
