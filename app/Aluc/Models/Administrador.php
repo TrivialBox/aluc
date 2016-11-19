616d02c605d8377d443ae99654f2a3340801c476
krysthyan <krysthyan_09h@yahoo.com>
2016-11-19 00:59:29 -0500

    Implemented methods

--- app/Aluc/Models/Administrador.php
+++ app/Aluc/Models/Administrador.php
@@ -1,5 +1,6 @@
 <?php
 namespace Aluc\Models;
+use Aluc\Dao\AdministradorDao;
 
 /**
  * Usuario que puede crear nuevos moderadores en el sistema.
@@ -9,16 +10,41 @@ class Administrador extends Persona {
         parent::__construct($id, $nombre);
     }
 
+    /**
+     * Metodo para obtener el objeto Administrador del array devuelto de la
+     * base de datos.
+     * @param $array
+     * @param bool $get_element
+     * @return Administrador|array
+     */
+    public static function get_object($array, $get_element = true){
+        if ($get_element){
+            return new Administrador($array[0]["id"], $array[0]['nombre']);
+
+        }else {
+            $admins = array();
+            foreach ($array as $fila){
+                array_push($admins,new Administrador($fila['id'], $fila['nombre']));
+            }
+            return $admins;
+        }
+    }
+
+    /**
+     * Metodo para obtener un administrador de la base de datos.
+     * @param $id
+     * @return Administrador|array
+     */
     public static function getInstance($id) {
+        return Administrador::get_object(AdministradorDao::getInstance()->get($id));
     }
 
-    public function save()
-    {
-        // TODO: Implement save() method.
-    }
-
-    public function delete()
-    {
-        // TODO: Implement delete() method.
+    /**
+     * Metodo para obtener una lista de todos los administradores.
+     * @param null $order_attribute
+     * @return Administrador|array
+     */
+    public static function getAll($order_attribute = null){
+        return Administrador::get_object(AdministradorDao::getInstance()->getAll($order_attribute), false);
     }
 }
