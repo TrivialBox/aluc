ad52f2e58ca0be0aabdad5c1838b762e21855ca7
krysthyan <krysthyan_09h@yahoo.com>
2016-11-19 17:49:40 -0500

    exception handling

--- app/Aluc/Dao/ModeradorDao.php
+++ app/Aluc/Dao/ModeradorDao.php
@@ -2,6 +2,10 @@
 namespace Aluc\Dao;
 
 
+use Aluc\Common\AlucException;
+use Aluc\Common\AlucRowException;
+use Herrera\Json\Exception\Exception;
+
 class ModeradorDao {
     private $database;
     private static $instance = null;
@@ -17,6 +21,7 @@ class ModeradorDao {
         }
         return static::$instance;
     }
+
     private function convertObjectArray($object){
         $array = [
             'id' => $object->id,
@@ -25,19 +30,35 @@ class ModeradorDao {
 
         return $array;
     }
+
     public function save($object, $type_save = true){
         if ($type_save){
-            $this->database->insert("moderador", $this->convertObjectArray($object));
+            try{
+                $this->database->insert("moderador", $this->convertObjectArray($object));
+            }catch (\Exception $e){
+                throw new AlucException(Database::getMgs($e->getCode(),$this->getModel()),$e->getMessage());
+            }
         } else {
-            $where = " id = '{$object->id}'";
-            $this->database->update('moderador',$this->convertObjectArray($object), $where);
+            try {
+                $where = " id = '{$object->id}'";
+                $this->database->update('moderador', $this->convertObjectArray($object), $where);
+            } catch (\Exception $e) {
+                throw new AlucException(Database::getMgs($e->getCode(),$this->getModel()), $e->getMessage());
+            }
         }
         return $this->get($object->id);
     }
 
     public function get($id){
+
         $where = "id = " . "'" . $id . "'";
         $moderador = $this->database->select("view_moderador", "*", $where, null);
+        if (count($moderador) === 0){
+            throw new AlucException(
+                Database::getMgs(5000,$this->getModel()),
+                "elemento no existe en la base de datos"
+            );
+        }
         return $moderador;
     }
 
@@ -53,6 +74,20 @@ class ModeradorDao {
 
     public function delete($id){
         $where = "id = " . "'" . $id . "'";
-        $this->database->delete("moderador", $where);
+        try{
+            $this->database->delete("moderador", $where);
+        }catch (\Exception $e){
+            throw new AlucException('Moderador eliminado', $e->getMessage());
+        }
+
+    }
+
+    private function getModel(){
+        return [
+            'clave_foranea' => ['usuario o moderador','registrado'],
+            'clave_pk_duplicate' => ['moderador'],
+            'elemento_null' => ['moderador','registrado']
+
+        ];
     }
 }
