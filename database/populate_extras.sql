5b76b6affaf0d4471442a13c595444543e1363e4
Santos Gallegos <santos_g@outlook.com>
2016-11-19 23:45:58 -0500

    populate extras

---
+++ database/populate_extras.sql
@@ -0,0 +1,23 @@
+-- Administradores
+INSERT INTO administrador (id) VALUES ('0105070353');
+INSERT INTO administrador (id) VALUES ('1400967715');
+
+-- Moderadores
+ INSERT INTO moderador (id, id_laboratorio) VALUES ('1400567572', '1');
+ INSERT INTO moderador (id, id_laboratorio) VALUES ('0105108336', '1');
+ INSERT INTO moderador (id, id_laboratorio) VALUES ('0104273131', '2');
+ INSERT INTO moderador (id, id_laboratorio) VALUES ('0105515472', '2');
+ INSERT INTO moderador (id, id_laboratorio) VALUES ('0104668009', '3');
+ INSERT INTO moderador (id, id_laboratorio) VALUES ('0103162038', '3');
+ INSERT INTO moderador (id, id_laboratorio) VALUES ('0604107607', '3');
+ INSERT INTO moderador (id, id_laboratorio) VALUES ('1400964001', '4');
+ INSERT INTO moderador (id, id_laboratorio) VALUES ('0105686737', '4');
+ INSERT INTO moderador (id, id_laboratorio) VALUES ('0104035514', '5');
+ INSERT INTO moderador (id, id_laboratorio) VALUES ('0105513402', '5');
+ INSERT INTO moderador (id, id_laboratorio) VALUES ('0106816952', '5');
+
+-- Lectores
+
+
+-- Reservas
+
