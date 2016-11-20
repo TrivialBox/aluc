894ebbb01692464a4d97cce1dd8f7d13c08fd99c
krysthyan <krysthyan_09h@yahoo.com>
2016-11-19 19:40:51 -0500

    manejo de excepciones

--- app/Aluc/Dao/LectorQrDao.php
+++ app/Aluc/Dao/LectorQrDao.php
@@ -1,24 +1,27 @@
 <?php
 
-namespace ALUC\Dao;
+namespace Aluc\Dao;
 
+
+use Aluc\Common\AlucException;
 
 class LectorQrDao {
 
     private $database;
     private static $instance = null;
 
-    private function __construct(){
+    private function __construct() {
         $this->database = new Database();
         $this->database->connect();
     }
 
-    public static function getInstance(){
-        if(static::$instance == null) {
+    public static function getInstance() {
+        if (static::$instance == null) {
             static::$instance = new self();
         }
         return static::$instance;
     }
+
 
     private function convertObjectArray($object){
         $array = [
@@ -33,10 +36,23 @@ class LectorQrDao {
 
     public function save($object, $type_save = true) {
         if ($type_save) {
-            $this->database->insert('lector', $this->convertObjectArray($object));
+
+            try{
+                $this->database->insert('lector', $this->convertObjectArray($object));
+
+            }catch (\Exception $e){
+
+                throw new AlucException(Database::getMgs($e->getCode(),$this->getModel()),$e->getMessage());
+            }
+
         } else {
-            $where = " mac = '{$object->mac}'";
-            $this->database->update('lector', $this->convertObjectArray($object), $where);
+            try{
+                $where = " mac = '{$object->mac}'";
+                $this->database->update('lector', $this->convertObjectArray($object), $where);
+            }catch (\Exception $e){
+                throw new AlucException(Database::getMgs($e->getCode(),$this->getModel()),$e->getMessage());
+            }
+
         }
         return $this->get($object->mac);
     }
@@ -44,6 +60,12 @@ class LectorQrDao {
     public function get($mac){
         $where = "mac = '{$mac}'";
         $lectorQr = $this->database->select("lector", "*", $where, null);
+        if (count($lectorQr) === 0){
+            throw new AlucException(
+                Database::getMgs(5000,$this->getModel()),
+                "elemento no existe en la base de datos"
+            );
+        }
         return $lectorQr;
     }
 
@@ -60,6 +82,15 @@ class LectorQrDao {
     public function delete($mac){
         $where = "mac = " . "'" . $mac . "'";
         $this->database->delete("lector", $where);
+    }
+
+    private function getModel(){
+        return [
+            'clave_foranea' => ['Lababoratorio','registrado'],
+            'clave_pk_duplicate' => ['Lector'],
+            'elemento_null' => ['Lector','registrado']
+
+        ];
     }
 }
 
