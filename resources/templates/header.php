d2b2fb84b0780fb5431b1a9b0d2b74e563275c6b
Santos Gallegos <santos_g@outlook.com>
2016-11-21 02:10:36 -0500

    Update header

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -1,24 +1,10 @@
+<!-- Required meta tags always come first -->
 <meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
 
 <!-- Bootstrap -->
-<!-- Local
-<link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-<script src="bower_components/bootstrap/dist/js/bootstrap.js"></script>
--->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<!-- Local
-<script src="bower_components/jquery/dist/jquery.js"></script>
--->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-<script src="https://craig.global.ssl.fastly.net/js/mousetrap/mousetrap.min.js?bc893"></script>
 
 <!-- Custom CSS -->
 <link rel="stylesheet" href="/css/custom.css">
-
