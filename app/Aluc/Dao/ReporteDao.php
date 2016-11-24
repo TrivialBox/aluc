4263d7870562e3866cd9a7de21b0f08a620052f2
krysthyan <krysthyan_09h@yahoo.com>
2016-11-24 16:24:17 -0500

    implementacion para obtener reportes de las reservas

--- app/Aluc/Dao/ReporteDao.php
+++ app/Aluc/Dao/ReporteDao.php
@@ -1,15 +1,129 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 24/11/16
- * Time: 14:25
- */
 
 namespace Aluc\Dao;
 
 
-class ReporteDao
-{
+class ReporteDao{
+    private $database;
+    private static $instance = null;
+
+
+    private function __construct() {
+        $this->database = new Database();
+        $this->database->connect();
+    }
+
+    public static function getInstance() {
+        if (static::$instance == null) {
+            static::$instance = new self();
+        }
+        return static::$instance;
+    }
+
+
+    public function getReportes(
+        $anio,
+        $id_usuario
+    ){
+        try{
+            $where = "year(fecha) = '{$anio}'";
+
+            if ($id_usuario != null){
+                $where .= " and id_usuario = '{$id_usuario}'";
+            }
+
+            return $reporteDia = $this->database
+                ->select(
+                    "view_reserva",
+                    '*',
+                    $where,
+                    'fecha asc'
+                );
+
+        }catch (\Exception $e){
+            ReservaDao::generarExcepcion($e);
+        }
+    }
+
+    public function getReporteDia(
+        $fecha,
+        $id_usuario,
+        $id_laboratorio
+    ){
+        try{
+            $where = "fecha = '{$fecha}'";
+            if ($id_usuario != null){
+                $where .= " and id_usuario = '{$id_usuario}'";
+            }
+            if ($id_laboratorio != null){
+                $where .= " and id_laboratorio = '{$id_laboratorio}'";
+            }
+            return $reporteDia = $this->database
+                ->select(
+                    "view_reserva",
+                    '*',
+                    $where,
+                    'fecha asc'
+                );
+
+        }catch (\Exception $e){
+            ReservaDao::generarExcepcion($e);
+        }
+    }
+
+    public function getReporteSemana($semana, $anio,$id_usuario, $id_laboratorio){
+        try{
+            $where = " week(fecha) = '{$semana}' 
+                    and  year(fecha) = '{$anio}'";
+
+            if ($id_usuario != null){
+                $where .= " and id_usuario = '{$id_usuario}'";
+            }
+            if ($id_laboratorio != null){
+                $where .= " and id_laboratorio = '{$id_laboratorio}'";
+            }
+            return $reporteDia = $this->database
+                ->select(
+                    "view_reserva",
+                    '*',
+                    $where,
+                    'fecha asc'
+                );
+
+        }catch (\Exception $e){
+            ReservaDao::generarExcepcion($e);
+        }
+    }
+
+    public function getReporteMes(
+        $month,
+        $anio,
+        $id_usuario,
+        $id_laboratorio
+    ){
+        try{
+            $where = "month(fecha) = '{$month}' 
+                    and  year(fecha) = '{$anio}'";
+
+            if ($id_usuario != null){
+                $where .= " and id_usuario = '{$id_usuario}'";
+            }
+            if ($id_laboratorio != null){
+                $where .= " and id_laboratorio = '{$id_laboratorio}'";
+            }
+            return $reporteDia = $this->database
+                ->select(
+                "   view_reserva",
+                    '*',
+                    $where,
+                    'fecha asc'
+                );
+
+        }catch (\Exception $e){
+            ReservaDao::generarExcepcion($e);
+        }
+    }
+
+
 
 }
--- app/Aluc/Models/Reporte.php
+++ app/Aluc/Models/Reporte.php
@@ -1,15 +1,86 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 24/11/16
- * Time: 14:16
- */
+
 
 namespace Aluc\Models;
 
+use Aluc\Dao\ReporteDao;
 
-class Reporte
-{
+class Reporte{
+
+    private static function getReserva($reserva){
+        if (count($reserva) > 0){
+            if (count($reserva) == 1){
+                return Reserva::get_object(
+                    $reserva,
+                    true
+                );
+            }else {
+                return Reserva::get_object(
+                    $reserva,
+                    false
+                );
+            }
+        }else{
+            return [];
+        }
+    }
+
+    public static function getInstance(
+        $anio,
+        $id_usuario =  null
+    ){
+        $reserva = ReporteDao::getInstance()
+            ->getReportes($anio, $id_usuario);
+        return self::getReserva($reserva);
+    }
+
+    public static function getReporteDia(
+        $fecha,
+        $id_usuario = null,
+        $id_laboratorio = null
+    ){
+        $reserva = ReporteDao::getInstance()
+            ->getReporteDia(
+                $fecha,
+                $id_usuario,
+                $id_laboratorio
+            );
+        return self::getReserva($reserva);
+    }
+
+    public static function getReporteSemana(
+        $num_semana,
+        $anio,
+        $id_usuario = null,
+        $id_laboratorio = null
+    ){
+        $reserva = ReporteDao::getInstance()
+            ->getReporteSemana(
+                $num_semana,
+                $anio,
+                $id_usuario,
+                $id_laboratorio
+            );
+        return self::getReserva($reserva);
+    }
+
+    public static function getReporteMes(
+        $num_mes,
+        $anio,
+        $id_usuario = null,
+        $id_laboratorio = null
+    ){
+        $reserva = ReporteDao::getInstance()
+            ->getReporteMes(
+                $num_mes,
+                $anio,
+                $id_usuario,
+                $id_laboratorio
+            );
+        return self::getReserva($reserva);
+    }
+
+
+
 
 }
