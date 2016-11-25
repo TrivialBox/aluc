2e170525cd521b7cbe4b1ca23431ce08da061fdb
krysthyan <krysthyan_09h@yahoo.com>
2016-11-24 23:51:58 -0600

    pagina 404

--- resources/templates/404.php
+++ resources/templates/404.php
@@ -6,10 +6,28 @@ header("HTTP/1.0 404 Not Found");
     <head>
         <meta charset="utf-8">
         <title>404</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     </head>
     <body>
-        <h1>404</h1>
-        <h2>Sorry :(</h2>
+    <div class="container-fluid">
+        <div class="row-fluid"><br>
+            <div class="col-lg-12"><br>
+                <div class="centering text-center"><brre    sssss>
+                    <div class="text-center">
+                        <div class="panel panel-danger ">
+                            <div class="panel-heading jumbotron">
+                                <h1 class="without-margin panel-title">No te preocupes.<br> Solo es error <span class="text-success"><big>404</big></span></h1>
+                            </div>
+                            <div class="panel-body">
+                                <h2 class="text-success">Página no encontrada</h2>
+                            </div>
+                            <div class="panel-footer">Mientras tanto visita <a href="www.trivialbox.com">Trivial Box</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     </body>
 </html>
 <?php
