fe6516a73b56cbbe640c2665d5c1c3322ca91b05
krysthyan <krysthyan_09h@yahoo.com>
2016-11-25 02:16:10 -0600

    generar reportes

--- resources/templates/reportes/html-to-pdf.php
+++ resources/templates/reportes/html-to-pdf.php
@@ -2,16 +2,16 @@
     <head>
         <style>
             table {
-                border-collapse: collapse;
+                border-spacing: 0.1rem;
                 width: 100%;
             }
 
             th, td {
                 text-align: left;
-                padding: 8px;
+                padding: 7px;
             }
 
-            tr:nth-child(even){background-color: #f2f2f2}
+            td:nth-child(even){background-color: #f2f2f2}
 
             th {
                 background-color: #00AAFF;
@@ -21,11 +21,20 @@
     </head>
     <body>
         <div>
-            <h1>Reporte</h1>
-            <table class="table">
+
+            <?php
+                setlocale(LC_TIME,"es_ES");
+                echo "<h2>" . $get('name') . "</h2>";
+                echo "<p>" . date('l jS \of F Y h:i:s A') . "</p>";
+            ?>
+            <table>
                 <thead>
                 <tr>
                     <?php
+                    $porcentaje = 100 / count($get('headers'));
+
+                    echo "<style>td, th {width: " . $porcentaje . "%}</style>";
+
                     foreach ($get('headers') as $header) {
                         echo "<th>{$header}</th>";
                     }
@@ -35,15 +44,23 @@
                 <tbody>
                 <?php
                 foreach ($get('rows') as $row) {
-                    echo '<tr>';
+                    echo "<tr >";
                     foreach ($row as $value) {
-                        echo "<td>{$value}</td>";
+                        echo "<td >{$value}</td>";
                     }
                     echo '</tr>';
                 }
                 ?>
                 </tbody>
             </table>
+            <br><br><br><br>
+            <p align="center">_____________________________</p>
+            <?php
+                echo "<p align=\"center\">" . $get('name_admin') . "</p>"
+
+            ?>
+
         </div>
     </body>
+
 </html>
