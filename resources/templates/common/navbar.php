89ed7931136a0ddcdf468750238f50e3b7503980
Santos Gallegos <santos_g@outlook.com>
2016-11-25 03:16:45 -0500

    Mostrar nombre y opción de salir en navbar
    closes #62

--- resources/templates/common/navbar.php
+++ resources/templates/common/navbar.php
@@ -22,13 +22,7 @@ use Aluc\Models\Usuario;
               ?>
               <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li class="dropdown-header">Nav header</li>
-            <li><a href="#">Separated link</a></li>
-            <li><a href="#">One more separated link</a></li>
+            <li><a href="#">Salir</a></li>
           </ul>
         </li>
       </ul>
