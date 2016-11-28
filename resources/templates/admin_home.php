4a7d3c12d6321049eb8215d4f27eca1366f954c5
Santos Gallegos <santos_g@outlook.com>
2016-11-28 18:49:01 -0500

    Eliminado archivo innecesario

--- resources/templates/admin_home.php
+++
@@ -1,9 +0,0 @@
-<h2>Panel administrativo</h2>
-<ul>
-    <li>
-        <a href="/admin/moderadores">Moderadores</a>
-    </li>
-    <li>
-        <a href="/admin/lectores-qr">Lectores QR</a>
-    </li>
-</ul>
