29475581ce3db77cb6d9a9334a923ba23df86a17
Santos Gallegos <santos_g@outlook.com>
2016-11-30 14:35:03 -0500

    Update populate_extras

--- database/populate_extras.sql
+++ database/populate_extras.sql
@@ -32,33 +32,31 @@ INSERT INTO lector(mac,id_laboratorio,ip,token) VALUES('00:22:49:00:01:02','6','
 
 -- Reservas
 
--- clases 
-CALL ALUC.insertar_reserva('0604107607','1','Clases normales ','10','clases','reservado','2016-11-25','15:00:00','17:00:00','Ds3lO3TT1UtO6RO');
-CALL ALUC.insertar_reserva('1400967715','2','Clases normales ','10','clases','reservado','2016-11-25','15:30:00','17:30:00','Ds3lO3TT1UtO6RO');
-CALL ALUC.insertar_reserva('0105070353','3','Clases normales ','10','clases','reservado','2016-11-25','15:00:00','17:00:00','Ds3lO3TT1UtO6RO');
-CALL ALUC.insertar_reserva('0104967864','4','Clases normales ','10','clases','reservado','2016-11-25','15:30:00','17:30:00','Ds3lO3TT1UtO6RO');
-CALL ALUC.insertar_reserva('0105850945','5','Clases normales ','10','clases','reservado','2016-11-25','15:00:00','17:00:00','Ds3lO3TT1UtO6RO');
-CALL ALUC.insertar_reserva('0104429527','6','Clases normales ','10','clases','reservado','2016-11-25','15:30:00','17:30:00','Ds3lO3TT1UtO6RO');
+-- clases
+CALL ALUC.insertar_reserva('0604107607','1','Clases normales ','10','clases','reservado','2016-12-05','15:00:00','16:00:00','Ds3lO3TT1UtO6RO');
+CALL ALUC.insertar_reserva('1400967715','2','Clases normales ','10','clases','reservado','2016-12-05','15:30:00','16:30:00','Ds3lO3TT1UtO6RO');
+CALL ALUC.insertar_reserva('0105070353','3','Clases normales ','10','clases','reservado','2016-12-05','15:00:00','16:00:00','Ds3lO3TT1UtO6RO');
+CALL ALUC.insertar_reserva('0104967864','4','Clases normales ','10','clases','reservado','2016-12-05','15:30:00','16:30:00','Ds3lO3TT1UtO6RO');
+CALL ALUC.insertar_reserva('0105850945','5','Clases normales ','10','clases','reservado','2016-12-05','15:00:00','16:00:00','Ds3lO3TT1UtO6RO');
+CALL ALUC.insertar_reserva('0104429527','6','Clases normales ','10','clases','reservado','2016-12-05','15:30:00','16:30:00','Ds3lO3TT1UtO6RO');
 
-CALL ALUC.insertar_reserva('0604107607','1','Clases normales ','10','clases','reservado','2016-11-26','15:00:00','17:00:00','Ds3lO3TT1UtO6RO');
-CALL ALUC.insertar_reserva('1400967715','2','Clases normales ','10','clases','reservado','2016-11-26','15:30:00','17:30:00','Ds3lO3TT1UtO6RO');
-CALL ALUC.insertar_reserva('0105070353','3','Clases normales ','10','clases','reservado','2016-11-26','15:00:00','17:00:00','Ds3lO3TT1UtO6RO');
-CALL ALUC.insertar_reserva('0104967864','4','Clases normales ','10','clases','reservado','2016-11-26','15:30:00','17:30:00','Ds3lO3TT1UtO6RO');
-CALL ALUC.insertar_reserva('0105850945','5','Clases normales ','10','clases','reservado','2016-11-26','15:00:00','17:00:00','Ds3lO3TT1UtO6RO');
-CALL ALUC.insertar_reserva('0104429527','6','Clases normales ','10','clases','reservado','2016-11-26','15:30:00','17:30:00','Ds3lO3TT1UtO6RO');
+CALL ALUC.insertar_reserva('0604107607','1','Clases normales ','10','clases','reservado','2016-12-05','10:00:00','11:00:00','Ds3lO3TT1UtO6RO');
+CALL ALUC.insertar_reserva('1400967715','2','Clases normales ','10','clases','reservado','2016-12-05','10:30:00','11:30:00','Ds3lO3TT1UtO6RO');
+CALL ALUC.insertar_reserva('0105070353','3','Clases normales ','10','clases','reservado','2016-12-05','10:00:00','11:00:00','Ds3lO3TT1UtO6RO');
+CALL ALUC.insertar_reserva('0104967864','4','Clases normales ','10','clases','reservado','2016-12-05','10:30:00','11:30:00','Ds3lO3TT1UtO6RO');
+CALL ALUC.insertar_reserva('0105850945','5','Clases normales ','10','clases','reservado','2016-12-05','10:00:00','11:00:00','Ds3lO3TT1UtO6RO');
+CALL ALUC.insertar_reserva('0104429527','6','Clases normales ','10','clases','reservado','2016-12-05','10:30:00','11:30:00','Ds3lO3TT1UtO6RO');
 
 
 
 -- reservas para prácticas
 
-CALL ALUC.insertar_reserva('0105513402','1','Prácticas realizadas para deberes','1','práctica','reservado','2016-11-25','07:00:00','09:00:00','Ds3lO3TT1UtO6RO');
-CALL ALUC.insertar_reserva('0105513402','2','Prácticas realizadas para deberes','5','práctica','reservado','2016-11-26','09:00:00','11:00:00','Ds3lO3TT1UtO6RO');
-CALL ALUC.insertar_reserva('1400567572','1','Prácticas realizadas para deberes','7','práctica','reservado','2016-11-25','07:00:00','09:00:00','Ds3lO3TT1UtO6RO');
-CALL ALUC.insertar_reserva('1400567572','2','Prácticas realizadas para deberes','2','práctica','reservado','2016-11-26','09:00:00','11:00:00','Ds3lO3TT1UtO6RO');
+CALL ALUC.insertar_reserva('0105513402','1','Prácticas realizadas para deberes','1','práctica','reservado','2016-12-4','07:00:00','08:00:00','Ds3lO3TT1UtO6RO');
+CALL ALUC.insertar_reserva('0105513402','2','Prácticas realizadas para deberes','5','práctica','reservado','2016-12-5','09:00:00','10:00:00','Ds3lO3TT1UtO6RO');
+CALL ALUC.insertar_reserva('1400567572','1','Prácticas realizadas para deberes','7','práctica','reservado','2016-12-4','07:00:00','08:00:00','Ds3lO3TT1UtO6RO');
+CALL ALUC.insertar_reserva('1400567572','2','Prácticas realizadas para deberes','2','práctica','reservado','2016-12-5','09:00:00','10:00:00','Ds3lO3TT1UtO6RO');
 
-CALL ALUC.insertar_reserva('0105108336','3','Prácticas realizadas para deberes','1','práctica','reservado','2016-11-25','09:00:00','11:00:00','Ds3lO3TT1UtO6RO');
-CALL ALUC.insertar_reserva('0105108336','4','Prácticas realizadas para deberes','5','práctica','reservado','2016-11-26','09:00:00','11:00:00','Ds3lO3TT1UtO6RO');
-CALL ALUC.insertar_reserva('0105515472','3','Prácticas realizadas para deberes','7','práctica','reservado','2016-11-25','11:00:00','13:00:00','Ds3lO3TT1UtO6RO');
-CALL ALUC.insertar_reserva('0105515472','4','Prácticas realizadas para deberes','2','práctica','reservado','2016-11-26','11:00:00','13:00:00','Ds3lO3TT1UtO6RO');
-
-
+CALL ALUC.insertar_reserva('0105108336','3','Prácticas realizadas para deberes','1','práctica','reservado','2016-12-4','09:00:00','10:00:00','Ds3lO3TT1UtO6RO');
+CALL ALUC.insertar_reserva('0105108336','4','Prácticas realizadas para deberes','5','práctica','reservado','2016-12-5','11:00:00','12:00:00','Ds3lO3TT1UtO6RO');
+CALL ALUC.insertar_reserva('0105515472','3','Prácticas realizadas para deberes','7','práctica','reservado','2016-12-4','09:00:00','10:00:00','Ds3lO3TT1UtO6RO');
+CALL ALUC.insertar_reserva('0105515472','4','Prácticas realizadas para deberes','2','práctica','reservado','2016-12-5','11:00:00','12:00:00','Ds3lO3TT1UtO6RO');
