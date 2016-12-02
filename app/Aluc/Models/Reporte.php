6f906fcdec83f11c04cd255498ae1e6ebccae6d7
krysthyan <krysthyan_09H@yahoo.com>
2016-12-01 23:59:28 -0500

    metodo getReportes (fechas por intervalo)

--- app/Aluc/Models/Reporte.php
+++ app/Aluc/Models/Reporte.php
@@ -3,6 +3,7 @@
 
 namespace Aluc\Models;
 
+use Aluc\Common\AlucException;
 use Aluc\Dao\ReporteDao;
 
 class Reporte{
@@ -77,6 +78,39 @@ class Reporte{
         return self::getReserva($reserva);
     }
 
+    public static function getReportes(
+        $fecha_inicial,
+        $fecha_final,
+        $id_usuario = null,
+        $id_laboratorio = null
+    ){
+        if (!self::validarFecha($fecha_inicial, $fecha_final)){
+            throw new AlucException(
+                "La fecha final es menor a la fecha inicial",
+                "La fecha final es menor a la fecha inicial"
+            );
+        }
+        $reserva = ReporteDao::getInstance()
+            ->getReporte(
+                $fecha_inicial,
+                $fecha_final,
+                $id_usuario,
+                $id_laboratorio
+            );
+        return self::getReserva($reserva);
+
+    }
+
+    private static function validarFecha(
+        $fecha_inicial,
+        $fecha_final
+    ){
+        if (strtotime($fecha_final) >= strtotime($fecha_inicial)){
+            return true;
+        }
+        return false;
+    }
+
     private static function getDiaActual(){
         return date("Y-n-j");
     }
@@ -92,8 +126,4 @@ class Reporte{
     private static function getSemanaActual(){
         return date("W");
     }
-
-
-
-
 }
