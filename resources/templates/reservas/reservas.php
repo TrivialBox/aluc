09f3b597b64842136c0a0701a200baa3f1462f19
Santos Gallegos <santos_g@outlook.com>
2016-12-01 22:03:18 -0500

    Load reservas on unique container

--- resources/templates/reservas/reservas.php
+++ resources/templates/reservas/reservas.php
@@ -67,18 +67,8 @@ use Aluc\Common\TemplateGenerator;
 
             <!-- Tab panes -->
             <div class="tab-content">
-                <div class="tab-pane fade in active" id="reservas-nuevas-content" role="tabpanel">
-                    <div class="container">
-                    </div>
+                <div class="tab-pane fade in active" id="reservas-content" role="tabpanel">
                 </div>
-                <div class="tab-pane fade in" id="reservas-pasadas-content" role="tabpanel">
-                    <!-- Reservas pasadas del pasado presente -->
-                </div>
-                <?php
-                if (empty($get('reservas'))) {
-                    TemplateGenerator::generate([], 'reservas/tip-container.php');
-                }
-                ?>
             </div>
             <!-- END panes -->
 
