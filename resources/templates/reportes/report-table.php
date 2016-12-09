0a4165f0553ec38eaf8a0b473e05d2ff319255df
Santos Gallegos <santos_g@outlook.com>
2016-12-09 02:24:40 -0500

    Acoplado pdf y html

--- resources/templates/reportes/report-table.php
+++ resources/templates/reportes/report-table.php
@@ -13,10 +13,25 @@
         foreach ($get('rows') as $row) {
             echo '<tr>';
             foreach ($row as $value) {
-                echo "<td>{$value}</td>";
+                echo '<td>';
+                if (is_array($value)) {
+                    echo '<dl>';
+                    foreach ($value as $name => $val) {
+                        echo <<<TAG
+                        <strong>{$name}:</strong>
+                        <span>{$val}</span>
+                        <br>
+TAG;
+                    }
+                    echo '</dl>';
+                } else {
+                    echo $value;
+                }
+                echo '</td>';
             }
             echo '</tr>';
         }
     ?>
     </tbody>
 </table>
+
